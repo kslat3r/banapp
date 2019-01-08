8af3eccc5b278a354b582fc68bfd16f71baabb7c
Ed Kelly <ed@newlinelabs.com>
2019-01-08 18:37:36 +0000

    Slight refactor

--- games/model.go
+++ games/model.go
@@ -3,6 +3,9 @@ package games
 import (
 	"banapp/common"
 	"errors"
+	"math/rand"
+	"reflect"
+	"time"
 
 	"github.com/globalsign/mgo/bson"
 )
@@ -63,10 +66,56 @@ func (game *game) update(data *gameData) (*game, error) {
 	}
 
 	if game.Status == statusWaiting && data.Status == statusPlaying {
-		if len(game.Players) >= 2 {
-			dealTiles(game)
-		} else {
+		if len(game.Players) < 2 {
 			return nil, errors.New("Game does not have enough players to begin")
+		}
+
+		rand.Seed(time.Now().Unix())
+
+		// get number of players
+
+		numPlayers := len(game.Players)
+
+		// get num of tiles
+
+		var numTiles int
+
+		if numPlayers >= 2 && numPlayers <= 4 {
+			numTiles = 21
+		} else if numPlayers > 4 && numPlayers <= 6 {
+			numTiles = 15
+		} else if numPlayers > 7 {
+			numTiles = 11
+		}
+
+		for _, player := range game.Players {
+			for i := 0; i < numTiles; i++ {
+				// get random index
+
+				keys := reflect.ValueOf(game.RemainingLetters).MapKeys()
+				random := rand.Int() % len(keys)
+
+				// get letter
+
+				letter := keys[random].String()
+
+				// add to player
+
+				if _, ok := player.Letters[letter]; !ok {
+					player.Letters[letter] = 0
+				}
+
+				// counts
+
+				player.Letters[letter]++
+				game.RemainingLetters[letter]--
+
+				// if remaining is zero, remove from map
+
+				if game.RemainingLetters[letter] == 0 {
+					delete(game.RemainingLetters, letter)
+				}
+			}
 		}
 	}
 
